27b27c44f2cc1799d7b21a7d807ffdd816bb53b7
woonki94 <woonki94@naver.com>
2022-01-16 18:00:40 +0900

    entity 설계

---
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -0,0 +1,16 @@
+package jpabook.jpashop.domain;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+@Getter
+public class Address {
+
+    private String city;
+    private String street;
+    private String zipcode;
+}

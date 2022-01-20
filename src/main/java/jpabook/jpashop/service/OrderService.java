0de3d75d40de7be427d6142375e521e2401526af
woonki94 <woonki94@naver.com>
2022-01-20 15:49:00 +0900

    주문 서비스

--- src/main/java/jpabook/jpashop/service/OrderService.java
+++ src/main/java/jpabook/jpashop/service/OrderService.java
@@ -8,6 +8,7 @@ import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import jpabook.jpashop.repository.MemberRepository;
 import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.OrderSearch;
 import lombok.RequiredArgsConstructor;
 import org.aspectj.weaver.ast.Or;
 import org.springframework.stereotype.Service;
@@ -57,9 +58,9 @@ public class OrderService {
     }
 
     //검색
-    //public List<Order> findOrders(OrderSearch orderSearch){
-    //    return orderRepository.findAll(orderSearch);
-    //}
+    public List<Order> findOrders(OrderSearch orderSearch){
+        return orderRepository.findAllByCriteria(orderSearch);
+    }
 
 
 }
